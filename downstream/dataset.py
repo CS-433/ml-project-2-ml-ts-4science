06dae7e01eb8a347dc51a035aea82ca3ceaf458b
MarioRicoIbanez <mario.ricoibanez@epfl.ch>
2024-11-24 00:17:04 +0100

    Refactor `dataset.py`, added `create_dataset` function to class to be more clear.

--- downstream/dataset.py
+++ downstream/dataset.py
@@ -5,10 +5,10 @@ import numpy as np
 import os
 
 class EmbeddingsDataset(torch.utils.data.Dataset):
-    def __init__(self, embeddings, coordinates, transform=False):
-        self.embeddings = embeddings      # Tensor de forma (N, E, 1024)
-        self.coordinates = coordinates    # Tensor de forma (N, E, ...)
+    def __init__(self, data_path, transform=False):
+        self.embeddings, self.coordinates = self.create_dataset(data_path)
         self.transform = transform
+        self.data_path = data_path
 
     def __len__(self):
         return self.embeddings.shape[0]   # N
@@ -30,29 +30,30 @@ class EmbeddingsDataset(torch.utils.data.Dataset):
     
 
 
+    def create_dataset(self, data_path):
+        embeddings = []
+        coordinates = []
+        for file in os.listdir(data_path):
+            if file.endswith(".npz"):
+                data = np.load(os.path.join(data_path, file))
+
+                embeddings.append(torch.tensor(data["embeddings"]))
+                coordinates.append(torch.tensor(data["coordinates"]))
+
+        embeddings = torch.stack(embeddings, dim=0)       # Shape: (N, E, 1024)
+        coordinates = torch.stack(coordinates, dim=0)    # Shape: (N, E, ...)
+
+        return embeddings, coordinates
+
+
 
 if __name__ == "__main__":
     # Example usage of the EmbeddingsDataset with BACH
     data_path = "/home/ricoiban/ml-project-2-ml-ts-4science/data/data/BACH/embeddings/embeddings_uni"
-    
-    # Load all the embeddings and coordinates from separate .npz files
-    embeddings = []
-    coordinates = []
-    for file in os.listdir(data_path):
-        if file.endswith(".npz"):
-            data = np.load(os.path.join(data_path, file))
 
-            embeddings.append(torch.tensor(data["embeddings"]))
-            coordinates.append(torch.tensor(data["coordinates"]))
-
-    # Stack the embeddings and coordinates along the first dimension (N)
-    embeddings = torch.stack(embeddings, dim=0)       # Shape: (N, E, 1024)
-    coordinates = torch.stack(coordinates, dim=0)    # Shape: (N, E, ...)
-
-    print(f"Embeddings shape: {embeddings.shape}")  # e.g., (N, E, 1024)
 
     # Create the dataset with the stacked tensors
-    dataset = EmbeddingsDataset(embeddings, coordinates, transform=True)
+    dataset = EmbeddingsDataset(data_path, transform=True)
     
     # Test if the dataset works correctly
     print("First sample in the dataset:")
